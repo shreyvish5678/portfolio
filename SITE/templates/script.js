893595e8f6a083dfa01266bf737e451643df069f
Shrey Vishen <shrey.vishen@gmail.com>
2024-01-09 23:06:56 -0800

    fix

--- SITE/templates/script.js
+++
@@ -1,6 +0,0 @@
-const img = document.querySelector('img');
-const button = document.querySelector('button');
-button.addEventListener("click", async function() {
-    const image_data = await fetch("/generate").then((res) => res.text());
-    img.src=`data:image/png;base64,${image_data}`
-});
